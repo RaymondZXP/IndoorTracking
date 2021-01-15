3cc012374b82ddbea2588d115409739d86c24258
Raymond <mshs.raymond.zhang@gmail.com>
2021-01-15 18:41:50 +0800

    transfer

---
+++ RTT/peripheral/uart/pca10100/blank/armgcc/_build/nrf52833_xxaa/nrf_assert.c.d
@@ -0,0 +1,131 @@
+_build/nrf52833_xxaa/nrf_assert.c.o: \
+ ../../../../../../components/libraries/util/nrf_assert.c \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdint.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdint.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_default_types.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/features.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_newlib_version.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_intsup.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52833.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_version.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/mpu_armv7.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52833.h \
+ ../../../../../../modules/nrfx/mdk/nrf52833_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_to_nrf52833.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdio.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/newlib.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/config.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/ieeefp.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/cdefs.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stddef.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdarg.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/reent.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_types.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_types.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/lock.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/types.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/types.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/stdio.h \
+ /home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdbool.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdint.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdint.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_default_types.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/features.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_newlib_version.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_intsup.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52833.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_version.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_compiler.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/mpu_armv7.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52833.h:
+
+../../../../../../modules/nrfx/mdk/nrf52833_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_to_nrf52833.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/stdio.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/newlib.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/config.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/ieeefp.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/cdefs.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stddef.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdarg.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/reent.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/_ansi.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_types.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/_types.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/lock.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/types.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/machine/types.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/arm-none-eabi/include/sys/stdio.h:
+
+/home/raymond/gcc-arm-none-eabi-9-2020-q2-update/lib/gcc/arm-none-eabi/9.3.1/include/stdbool.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
